7c8f51ecae6ea43ec95a6dfda26898936f8358fc
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2024-03-09 20:06:43 +0530

    video crowd analysis ui integration

--- backend/annotater.py
+++ backend/annotater.py
@@ -1,52 +1,112 @@
+import os
 from fastapi import FastAPI, File, UploadFile
-from PIL import Image
-import io
 import cv2
-import base64
 import supervision as sv
-from ultralytics import YOLO 
+from ultralytics import YOLO
 import numpy as np
-
-model = YOLO("yolov8n.pt")  # Load your YOLOv8 model
-model.to(device='cuda')
 app = FastAPI()
 
-@app.post('/annotate')
-async def annotate_image(file: UploadFile = File(...)):
-    image_bytes = await file.read()
+@app.post("/upload_video/{camera_id}")
+async def upload_video(camera_id: int, video: UploadFile = File(...)):
+    # Generate a unique filename for the uploaded video
+    _, ext = os.path.splitext(video.filename)
+    filename = f"camera_{camera_id}_video{ext}"
+    
+    # Save the uploaded video to a temporary folder
+    file_path = os.path.join("temp/", filename)
+    
+    with open(file_path, "wb") as file:
+        content = await video.read()
+        file.write(content)
+    
+    return {"message": "Video uploaded successfully"}
 
-    # Convert bytes to OpenCV image format
-    nparr = np.frombuffer(image_bytes, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  
+@app.post("/analyze_crowd/{camera_id}")
+async def analyze_crowd(camera_id: int, data: dict):
+    # Load the video from the temporary folder
+    file_path = f"temp/camera_{camera_id}_video.mp4"
+    video = cv2.VideoCapture(file_path)
+    
+    # Load the YOLOv5 model
+    model = YOLO("models/yolov8n.pt")
 
-    # Perform YOLO inference
-    results = model(image)[0]
-    detections = sv.Detections.from_ultralytics(results)
+    heat_map_annotator = sv.HeatMapAnnotator(
+        position=sv.Position.BOTTOM_CENTER,
+        opacity=0.5,
+        radius=25,
+        kernel_size=25,
+        top_hue=0,
+        low_hue=125,
+    )
+    label_annotator = sv.LabelAnnotator(text_position=sv.Position.CENTER)
+    corner_annotator = sv.BoxCornerAnnotator()
+    trace_annotator = sv.TraceAnnotator()
+    cap = cv2.VideoCapture(file_path)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    cap.release()
 
-    # Create annotators
-    bounding_box_annotator = sv.BoundingBoxAnnotator()
-    label_annotator = sv.LabelAnnotator()
+    byte_tracker = sv.ByteTrack(
+        track_thresh=0.35,
+        track_buffer=5 * fps,
+        match_thresh=0.99,
+        frame_rate=fps,
+    )
+    video_info = sv.VideoInfo.from_video_path(video_path=file_path )
+    frames_generator = sv.get_video_frames_generator(
+        source_path=file_path , stride=1
+    )
+    with sv.VideoSink(target_path=f"result/camera_{camera_id}_video.mp4", video_info=video_info,codec="H264") as sink:
+        for frame in frames_generator:
+            result = model(
+                source=frame,
+                classes=[0],  # only person class
+                conf=data["confidence_level"],
+                iou=0.5,
+                # show_conf = True,
+                # save_txt = True,
+                # save_conf = True,
+                # save = True,
+                device=0,  # use None = CPU, 0 = single GPU, or [0,1] = dual GPU
+            )[0]
 
-    # Get labels
-    labels = [
-        model.model.names[class_id]
-        for class_id
-        in detections.class_id
-    ]
+            detections = sv.Detections.from_ultralytics(result)  # get detections
 
-    # Annotate the image
-    annotated_image = bounding_box_annotator.annotate(
-        scene=image, detections=detections)
-    annotated_image = label_annotator.annotate(
-        scene=annotated_image, detections=detections, labels=labels)
+            detections = byte_tracker.update_with_detections(
+                detections
+            )  # update tracker
 
-    # Convert annotated image back to bytes for sending
-    # Or '.png' if needed
+            annotated_frame = frame.copy()  # Start with a fresh copy in every iteration
 
-    ret, buffer = cv2.imencode('.jpg', annotated_image)  
-    image_bytes = buffer.tobytes()
+            if data["heatmap"]:
+                annotated_frame = heat_map_annotator.annotate(
+                    scene=annotated_frame, detections=detections
+                )
 
-    # Encode as base64
-    image_base64 = base64.b64encode(image_bytes).decode('utf-8')  
+            if data["trace"]:
+                annotated_frame = trace_annotator.annotate(
+                    scene=annotated_frame, detections=detections
+                )
 
-    return {"image": image_base64} 
+            if data["bounding_boxes"]:
+                annotated_frame = corner_annotator.annotate(
+                    scene=annotated_frame, detections=detections
+                )
+            if data["label"]:
+            ### draw other attributes from `detections` object
+                labels = [
+                    f"#{tracker_id}"
+                    for class_id, tracker_id in zip(
+                        detections.class_id, detections.tracker_id
+                    )
+                ]
+                
+                label_annotator.annotate(
+                    scene=annotated_frame, detections=detections, labels=labels
+                )
+
+            sink.write_frame(frame=annotated_frame)
+    # Perform crowd analysis
+    print(data)
+    
+    # Return the results
+    return {"message": "Crowd analysis complete"}
--- frontend/components/camera_component.py
+++ frontend/components/camera_component.py
@@ -1,23 +1,56 @@
 import streamlit as st
+import requests
+from io import BytesIO
 
 def render_camera_component(camera_id):
+    
     st.header(f"Camera {camera_id+1}")
     st.divider()
-    camera_key = f"camera_{camera_id}"  # Unique key based on camera_id
-
-    st.image("assets/placeholder.jpg")
+    camera_key = f"camera_{camera_id}"  # Unique key based on camera_id  # Display the video
     with st.expander("Camera Settings"):
-        source_type = st.selectbox("Source", ["Image", "Video", "YouTube Video", "IP Camera", "Webcam"], key=f"{camera_key}_source_type")
-        if source_type in ["Image", "Video"]:
-            uploaded_file = st.file_uploader("Upload File", type=["jpg", "png", "mp4"], key=f"{camera_key}_file_uploader")
-            if uploaded_file is not None:
-                pass  # Do something with the uploaded file (e.g., pass to your backend)
-
+        source_type = st.selectbox("Source", ["Video", "YouTube Video", "IP Camera", "Webcam"], key=f"{camera_key}_source_type")
+        if source_type == "Video":
+            uploaded_file = st.file_uploader("Upload File", type=['mp4', 'mov', 'avi'], key=f"{camera_key}_file_uploader")
+            if st.button("Upload", key=f"{camera_key}_upload_button"):
+                if uploaded_file is not None:
+                    with st.spinner("Uploading video..."):
+                        # Send the uploaded video to the backend
+                        files = {'video': (uploaded_file.name, uploaded_file.getvalue())}
+                        response = requests.post(f"http://localhost:8000/upload_video/{camera_id}", files=files)
+                        if response.status_code == 200:
+                            st.success("Video uploaded successfully!")
+                        else:
+                            st.error("Failed to upload video.")
         elif source_type in ["YouTube Video", "IP Camera"]:
             link = st.text_input("Enter Link", key=f"{camera_key}_link")
             if link:
                 pass
                 # Do something with the link (e.g., validate, pass to backend)
         confidence_level = st.slider("Confidence Level", 0.0, 1.0, 0.75, key=f"{camera_key}_confidence_level")
-        filter_types = st.multiselect("Filters", ["Crowd Density", "Threat", "Anomaly"], key=f"{camera_key}_filter_types")
-    st.button("Start Analysis", key=f"{camera_key}_start_analysis")
+        filter_types = st.selectbox("Filters", ["Crowd Density", "Threat", "Anomaly"], key=f"{camera_key}_filter_types")
+        if "Crowd Density" in filter_types:
+            st.subheader("Crowd Management Options")
+            st.checkbox("Bounding Boxes", key=f"{camera_key}_bounding_boxes")
+            st.checkbox("Heatmap", key=f"{camera_key}_heatmap")
+            st.checkbox("Trace", key=f"{camera_key}_trace")
+            st.checkbox("Label", key=f"{camera_key}_label")
+    if st.button("Start Analysis", key=f"{camera_key}_start_analysis"):
+    # Collect the necessary settings from Streamlit session state
+        data = {
+            'confidence_level': confidence_level,
+            'bounding_boxes': st.session_state.get(f"{camera_key}_bounding_boxes", False),  # Corrected  
+            'heatmap': st.session_state.get(f"{camera_key}_heatmap", False),  
+            'trace': st.session_state.get(f"{camera_key}_trace", False),  
+            'label': st.session_state.get(f"{camera_key}_label", False) 
+        }
+
+        # Ensure that the video has been uploaded before starting the analysis
+        if uploaded_file:
+            with st.spinner("Analyzing video..."):
+                response = requests.post(f"http://localhost:8000/analyze_crowd/{camera_id}", json=data)  # API call change
+                if response.status_code == 200:
+                    result = response.json()  # Retrieve results in JSON format
+                    st.success("Crowd analysis complete!")
+                    st.video(f"../backend/result/camera_{camera_id}_video.mp4")  # Display received results
+                else:
+                    st.error("Failed to start crowd analysis.")
